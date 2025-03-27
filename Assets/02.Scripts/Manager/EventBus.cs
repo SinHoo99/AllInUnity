418591b7a4e89c03e69de6e6f2f42423a3854b68
SinHoo99 <tjdqls991218@gmail.com>
2025-03-27 20:38:40 +0900

    Update EventBus.cs

--- Assets/02.Scripts/Manager/EventBus.cs
+++ Assets/02.Scripts/Manager/EventBus.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public static class EventBus
 {
@@ -40,7 +41,8 @@ public static class EventBus
     }
 }
 # region 이벤트 버스 사용예시
-/*// 1. 이벤트 데이터 타입 정의
+// 1. 이벤트 데이터 타입 정의
+// 이벤트 발생시 전달할 데이터 구조 만들어야됨
 public class PlayerDieEvent
 {
     public string playerName;
@@ -82,6 +84,103 @@ public class UIManager : Singleton<UIManager>
         Debug.Log("플레이어 사망" + e.playerName);
         gameOverPanel.SetActive(true);
     }
-}*/
+}
 
+#endregion
+
+#region 이벤트 버스활용 컴포넌트 독립화 과정 만들어보기
+public class AObject : MonoBehaviour
+{
+    public GameObject AObj;
+    public Text AObjTxt;
+
+    private void OnEnable()
+    {
+        EventBus.Subscribe<AObjectEvent>(OnAobjectEvent);
+    }
+
+    private void OnDisable()
+    {
+        EventBus.Unsubscribe<AObjectEvent>(OnAobjectEvent);
+    }
+
+    void OnAobjectEvent(AObjectEvent e)
+    {
+        Debug.Log("플레이어 사망" + e.playerName);
+        AObjTxt.text = e.playerName;
+        AObj.SetActive(true);
+    }
+}
+public class BObject : MonoBehaviour
+{
+    public GameObject BObj;
+    public Text BObjTxt;
+
+    private void OnEnable()
+    {
+        EventBus.Subscribe<AObjectEvent>(OnBobjectEvent);
+    }
+
+    private void OnDisable()
+    {
+        EventBus.Unsubscribe<AObjectEvent>(OnBobjectEvent);
+    }
+
+    void OnBobjectEvent(AObjectEvent e)
+    {
+        Debug.Log("플레이어 사망" + e.playerName);
+        BObjTxt.text = e.playerName;
+        BObj.SetActive(true);
+    }
+}
+public class CObject : MonoBehaviour
+{
+    public GameObject CObj;
+    public Text CObjTxt;
+
+    private void OnEnable()
+    {
+        EventBus.Subscribe<AObjectEvent>(OnCobjectEvent);
+    }
+
+    private void OnDisable()
+    {
+        EventBus.Unsubscribe<AObjectEvent>(OnCobjectEvent);
+    }
+
+    void OnCobjectEvent(AObjectEvent e)
+    {
+        Debug.Log("플레이어 사망" + e.playerName);
+        CObjTxt.text = e.playerName;
+        CObj.SetActive(true);
+    }
+}
+
+
+public class AObjectEvent
+{
+    public string playerName;
+    public AObjectEvent(string name)
+    {
+        playerName = name;
+    }
+}
+
+public class BObjectEvent
+{
+    public string playerName;
+    public BObjectEvent(string name)
+    {
+        playerName = name;
+    }
+}
+
+public class CObjectEvent
+{
+    public string playerName;
+    public CObjectEvent(string name)
+    {
+        playerName = name;
+    }
+}
 #endregion
